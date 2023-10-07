f97a698d7a57d399a47fbc0a807c2dcf94deffd7
Jenny Belanger <jbelangr@gmail.com>
2023-10-07 15:33:03 -0400

    Fix tests

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -1,20 +1,20 @@
 import 'cypress-file-upload'; // eslint-disable-line import/no-extraneous-dependencies
 
 Cypress.Commands.add('build', () => {
-	const cssFolder = `${Cypress.env('project_path')}/build/css`;
+	const cssFolder = `${Cypress.env('project_path')}/assets/css`;
 	cy.exec(`if [! -f ${cssFolder}/style.min.css]; then (cd ${Cypress.env('project_path')} && yarn install && yarn build); fi`);
 });
 
 Cypress.Commands.add('setUploads', (uploadsFolder = 'cypress/fixtures/original') => {
-	cy.exec(`mkdir -p ${Cypress.env('project_path')}/build/images`);
-	cy.exec(`rm -r ${Cypress.env('project_path')}/build/images`);
-	cy.exec(`cp -r ${uploadsFolder} ${Cypress.env('project_path')}/build/images`);
+	cy.exec(`mkdir -p ${Cypress.env('project_path')}/assets/images`);
+	cy.exec(`rm -r ${Cypress.env('project_path')}/assets/images`);
+	cy.exec(`cp -r ${uploadsFolder} ${Cypress.env('project_path')}/assets/images`);
 });
 
 Cypress.Commands.add('resetJson', () => {
-	cy.exec(`mkdir -p ${Cypress.env('project_path')}/build/json`);
-	cy.exec(`rm -f ${Cypress.env('project_path')}/build/json/folders.json`);
-	cy.exec(`rm -f ${Cypress.env('project_path')}/build/json/images.json`);
+	cy.exec(`mkdir -p ${Cypress.env('project_path')}/assets/json`);
+	cy.exec(`rm -f ${Cypress.env('project_path')}/assets/json/folders.json`);
+	cy.exec(`rm -f ${Cypress.env('project_path')}/assets/json/images.json`);
 });
 
 Cypress.Commands.add('setupApi', () => {
